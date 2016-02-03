0161f451c6fda43744c299a4c10397c0c6e47d1c
anthony <thony_prz@outlook.com>
2016-02-03 08:28:18 +0000

    03/02/16

--- controllers/configuracion.js
+++ controllers/configuracion.js
@@ -1,9 +1,9 @@
 var schema =require("../models/users.js");
-<<<<<<< HEAD
+//<<<<<<< HEAD
 var EstadisticaSemana= require('../routes/estadisticaSemana.js');
-=======
+//=======
 
->>>>>>> 493ed6a55321ca7d8050603630dfc0c647ffd945
+//>>>>>>> 493ed6a55321ca7d8050603630dfc0c647ffd945
 exports.configuracion = function (req, res) {
   
   
@@ -15,10 +15,10 @@ schema.update({usuario: req.session.a}, {alertas: alertas, tiempoDeAlertas: tiem
   
    
    req.session.dispositivo = idDispositivo;
-<<<<<<< HEAD
+//<<<<<<< HEAD
     EstadisticaSemana.Semana(req,res, req.session.dispositivo);
-=======
->>>>>>> 493ed6a55321ca7d8050603630dfc0c647ffd945
+//=======
+//>>>>>>> 493ed6a55321ca7d8050603630dfc0c647ffd945
     res.redirect("/home");
    
    console.log("configuracion actualizado");
